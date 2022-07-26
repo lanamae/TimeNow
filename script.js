03fc8e2e46b860418d01f384cd68c5ce8c99a244
Lana Mae Galvez <lanamaegalvez15@gmail.com>
2022-07-26 20:10:59 +0800

    Update _mobile.scss and added script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const toggle = document.querySelector('.toogle.menu')
+const menuBar = document.querySelector('.menu-bar')
+const close = document.querySelector('#close')
+
+const showMenu = () =>{
+    menuBar.style.display = 'block';
+    
+}
+
+const closeMenu = () =>{
+    menuBar.style.display = 'none';
+}
