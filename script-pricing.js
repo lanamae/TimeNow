1a1d12856c90683c4985ce6925172c0111f2f6be
Lana Mae Galvez <lanamaegalvez15@gmail.com>
2022-08-01 12:56:56 +0800

    Update -mobile.scss

--- script-pricing.js
+++ script-pricing.js
@@ -1,18 +1,24 @@
 const toggle = document.querySelector('.toggle-menu')
-const close = document.querySelector('#close')
+
 const menuBar = document.querySelector('.menu-bar')
 
+
+const closeMenu = document.querySelector('.closeMenu');
 
 toggle.addEventListener('click',  () =>{
     // console.log('event is working')
     menuBar.classList.add('show')
     menuBar.classList.remove('hide')
-    toggle.style.display = 'none'
 })
 
-close.addEventListener('click',  () =>{
-    console.log('closeevent is working')
+
+
+closeMenu.addEventListener('click', () =>{
+    console.log('hello')
     menuBar.classList.remove('show')
     menuBar.classList.add('hide')
-    
 })
+
+
+
+
